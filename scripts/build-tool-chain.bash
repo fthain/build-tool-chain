329316b3698c3fa3f9a17ad2a68fa8ed2b707b00
Finn Thain <fthain@telegraphics.com.au>
2018-10-20 16:44:05 +1100

    Contents of btc-0.11-20070822.tar.bz2

--- scripts/build-tool-chain.bash
+++ scripts/build-tool-chain.bash
@@ -4,24 +4,6 @@
 # Copyright (c) 2004-2007 Finn Thain
 # fthain@telegraphics.com.au
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
- 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
- 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-
 set -e -u
 
 ##############################################
@@ -202,14 +184,14 @@ if [ ${BUILD} != ${BUILD/%-apple-darwin*} ] ; then
     log install_host_tool gettext-0.16.1
     log install_host_tool gawk-3.1.5
     log install_host_tool bison-1.28
-    log install_coreutils coreutils-5.94
+    log install_coreutils coreutils-6.9
     log install_loadkeys  kbd-1.12
 else
     log install_host_tool gawk-3.1.5
     log install_host_tool bison-1.28
     log install_flex      flex-2.5.4a
 fi
-log install_depmod depmod.pl-14592
+log install_depmod depmod.pl-19079
 
 rm -rf ${HOST_TOOLS_PREFIX}/share/{emacs,doc,aclocal}
 
